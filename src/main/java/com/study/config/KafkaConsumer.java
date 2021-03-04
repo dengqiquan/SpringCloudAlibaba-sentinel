9bced2356ae4456b7a1507bffa3ad15b13a6e073
XC17335 <dengqiquan@xinchao.com>
2021-03-04 16:38:13 +0800

    引入kafka

--- src/main/java/com/study/config/KafkaConsumer.java
+++ src/main/java/com/study/config/KafkaConsumer.java
@@ -24,34 +24,36 @@ public class KafkaConsumer {
      * 定义此消费者接收topics = "demo"的消息，与controller中的topic对应上即可
      * @param record 变量代表消息本身，可以通过ConsumerRecord<?,?>类型的record变量来打印接收的消息的各种信息
      */
-    @KafkaListener(groupId = "group-demo", topics = "iris")
-    public void userListener(ConsumerRecord<?,?> record, Acknowledgment ack){
-        System.out.println("topic:iris 收到了一条消息");
-        System.out.println(record.topic());
-        System.out.println(record.offset());
-        System.out.println(record.value());
-        ack.acknowledge();
-    }
+//    @KafkaListener(groupId = "group-demo", topics = "iris")
+//    public void userListener(ConsumerRecord<?,?> record, Acknowledgment ack){
+//        System.out.println("topic:iris 收到了一条消息");
+//        System.out.println(record.topic());
+//        System.out.println(record.offset());
+//        System.out.println(record.value());
+//        ack.acknowledge();
+//    }
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @KafkaListener(topics = "demo",groupId = "group-one")
-    public void topic_one(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
+    public void topic_one(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,Acknowledgment ack){
 
         Optional message=Optional.ofNullable(record.value());
         if (message.isPresent()){
             Object msg=message.get();
-            logger.info("被"+"iris"+"消费了： +++++++++++++++ Topic:" + topic+",Record:" + record+",Message:" + msg);
+            logger.info("被"+"demo"+"消费了： +++++++++++++++ Topic:" + topic+",Record:" + record+",Message:" + msg);
+//            ack.acknowledge();
         }
     }
 
     @KafkaListener(topics = "iris",groupId="group-one")
-    public void topic_two(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
+    public void topic_two(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,Acknowledgment ack){
 
         Optional message=Optional.ofNullable(record.value());
         if (message.isPresent()){
             Object msg=message.get();
             logger.info("被"+"iris"+"1111消费了： +++++++++++++++ Topic:" + topic+",Record:" + record+",Message:" + msg);
+            ack.acknowledge();
         }
     }
 
